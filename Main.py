48aa52d3bfc3336054787797439f86709b1b72aa
yusufyildirim <yyldrm2525@gmail.com>
2021-12-23 15:51:32 +0300

    created first project

---
+++ Main.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import modules.color as col
+
+width = 720
+height = 640
+
+pg.init()
+screen = pg.display.set_mode((width,height))
+
+pg.display.set_caption("Marmara chess")
+while True:
+    pass
